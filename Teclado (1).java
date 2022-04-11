123d6fc4c22cd5186e2854ec236989b221d6f218
Eduarda Pagnussat <85207171+eduardapagnussat@users.noreply.github.com>
2022-04-11 15:29:16 -0300

    Update Teclado (1).java

--- Teclado (1).java
+++ Teclado (1).java
@@ -7,7 +7,7 @@ public class Teclado {
     public float valorMedio;
     public float peso; 
     
-
+// Construtores (adicionar o restante dos atributos)
     public Teclado(){}
 
     public Teclado(String marca, float valorMedio){
